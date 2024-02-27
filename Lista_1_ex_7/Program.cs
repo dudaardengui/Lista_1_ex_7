c5ddeffe9ceb2c873ce04a87e62a547eaa9b90aa
ALUNO <ALUNO@LABHPC06>
2024-02-27 15:08:54 -0300

    Adicionar arquivos de projeto.

---
+++ Lista_1_ex_7/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_1_ex_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.WriteLine("Digite um número inteiro:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número inteiro:");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite mais um número inteiro:");
+            c = int.Parse(Console.ReadLine());
+            if (a > b && b > c)
+            {
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                Console.WriteLine(c);
+            }
+            else if (a > c && c > b)
+            {
+                Console.WriteLine(a);
+                Console.WriteLine(c);
+                Console.WriteLine(b);
+            }
+            else if (b > a && a > c)
+            {
+                Console.WriteLine(b);
+                Console.WriteLine(a);
+                Console.WriteLine(c);
+            }
+            else if (b > c && c > a)
+            {
+                Console.WriteLine(b);
+                Console.WriteLine(c);
+                Console.WriteLine(a);
+            }
+            else if (c > a && a > b)
+            {
+                Console.WriteLine(c);
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+            }
+            else
+            {
+                Console.WriteLine(c);
+                Console.WriteLine(b);
+                Console.WriteLine(a);
+            }
+            Console.ReadKey();
+        }
+    }
+}
